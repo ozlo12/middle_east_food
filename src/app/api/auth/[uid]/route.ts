af7a3d72aab37ba87ebf7ae554494039a89a77b8
Amir Sameh <amir_sameh@yahoo.com>
2023-07-01 09:35:56 +0300

    firebase-env

--- src/app/api/auth/[uid]/route.ts
+++ src/app/api/auth/[uid]/route.ts
@@ -2,12 +2,21 @@ import { NextResponse } from "next/server";
 import { applicationDefault, getApps, initializeApp } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { getDatabase } from "firebase-admin/database";
+import { credential } from "firebase-admin";
 
 async function isAdmin(uid: string) {
+  const stringServiceAccount = process.env.FIREBASE_SERVICE_ACCOUNT_KEY;
+
+  if (!stringServiceAccount)
+    throw new Error("Service account variable not exist!");
+
+  const serviceAccount = JSON.parse(stringServiceAccount);
+
   const app =
     getApps()[0] ||
     initializeApp({
-      credential: applicationDefault(),
+      // credential: applicationDefault(),
+      credential: credential.cert(serviceAccount),
       databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DB_URL,
     });
 
@@ -16,16 +25,16 @@ async function isAdmin(uid: string) {
 
   if (!user) return false;
 
-  if(user.customClaims?.["admin"])return true;
+  if (user.customClaims?.["admin"]) return true;
 
   const db = getDatabase(app);
-  
-  const adminRef = db.ref('/admins/'+uid);
 
-  const isAdmin = (await adminRef.get()).exists()
+  const adminRef = db.ref("/admins/" + uid);
+
+  const isAdmin = (await adminRef.get()).exists();
 
   if (isAdmin) {
-    auth.setCustomUserClaims(uid,{admin:true})
+    auth.setCustomUserClaims(uid, { admin: true });
     return true;
   }
 
@@ -40,7 +49,7 @@ export async function GET(
     const result = await isAdmin(uid);
     return NextResponse.json({ isAdmin: result }, { status: 200 });
   } catch (err) {
-    console.log(err instanceof Error?err.message:err);
+    console.log(err instanceof Error ? err.message : err);
     return NextResponse.json({}, { status: 401 });
   }
 }
